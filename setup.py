b2165d5eb7417dbdf465d68f273ec253be602631
joamag <joamag@gmail.com>
2015-08-09 21:34:29 +0100

    new client naming

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setuptools.setup(
     version = "0.1.0",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
-    description = "Nexmo API",
+    description = "Nexmo API Client",
     license = "GNU General Public License (GPL), Version 3",
     keywords = "nexmo api",
     url = "http://nexmo_api.hive.pt",
